26dbfedc2488285d73397bfd0979defd02a96516
Jorgc <condori.jrge@gmail.com>
2022-01-04 00:11:42 -0300

    59

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -18,20 +18,37 @@ const ExpenseForm = () => {
         setInsertedTitle(event.target.value);
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: insertedTitle,
+            amount: insertedAmount,
+            date: insertedDate
+        }
+        console.log(expenseData);
+
+        setInsertedTitle('');
+        setInsertedAmount('');
+        setInsertedDate('')
+
+
+    }
+
+
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler}></input>
+                    <input type='text' value={insertedTitle} onChange={titleChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='text' min="0.01" step="0.01"></input>
+                    <input type='text' min="0.01" step="0.01" value={insertedAmount} onChange={amountChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31"></input>
+                    <input type='date' min="2019-01-01" max="2022-12-31" value={insertedDate} onChange={dateChangeHandler}></input>
                 </div>
             </div>
             <div className='new-expense__actions'>
